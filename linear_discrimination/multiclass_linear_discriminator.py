91102026c50147b369da34498c8db5bf01bbb935
orhan <ozalp.orhan@gmail.com>
2013-12-11 15:59:57 +0200

    removed test method

--- linear_discrimination/multiclass_linear_discriminator.py
+++ linear_discrimination/multiclass_linear_discriminator.py
@@ -1,10 +1,8 @@
-import random
 import numpy as np
-import matplotlib.pyplot as plt
 from linear_disriminator import LinearDiscriminator
 
 
-class MulticlassLinearDiscriminator:
+class MultiClassLinearDiscriminator:
 
     def __init__(self, k, d):
         self.k = k
@@ -23,54 +21,3 @@ class MulticlassLinearDiscriminator:
 
             for i in xrange(self.d + 1):
                 self.weights.itemset((ci, i), ld.weights.item((i,)))
-
-
-def main():
-    n = 15
-    k = 3
-    d = 2
-    colors = ['b', 'r', 'g']
-    input_matrix = np.asmatrix(np.zeros((n, d + 1)))
-
-    for i in xrange(n):
-        input_matrix.itemset((i, 0), 1)
-
-    for i in xrange(n/3):
-        input_matrix.itemset((i, 1), random.randint(25, 50) / 100.0)
-        input_matrix.itemset((i, 2), random.randint(25, 50) / 100.0)
-        plt.plot([input_matrix.item((i, 1))], [input_matrix.item((i, 2))], colors[0] + 'o')
-
-    for i in xrange(n/3, (n*2)/3):
-        input_matrix.itemset((i, 1), random.randint(125, 150) / 100.0)
-        input_matrix.itemset((i, 2), random.randint(125, 150) / 100.0)
-        plt.plot([input_matrix.item((i, 1))], [input_matrix.item((i, 2))], colors[1] + 'o')
-
-    for i in xrange((2*n)/3, n):
-        input_matrix.itemset((i, 1), random.randint(25, 50) / 100.0)
-        input_matrix.itemset((i, 2), random.randint(125, 150) / 100.0)
-        plt.plot([input_matrix.item((i, 1))], [input_matrix.item((i, 2))], colors[2] + 'o')
-
-    result_vector = np.zeros((n, k))
-
-    for i in xrange(n/3):
-        result_vector.itemset((i, 0), 1)
-    for i in xrange(n/3, (2*n)/3):
-        result_vector.itemset((i, 1), 1)
-    for i in xrange((2*n)/3, n):
-        result_vector.itemset((i, 2), 1)
-
-    ld = MulticlassLinearDiscriminator(k, d)
-    ld.train(input_matrix, result_vector)
-
-    for i in xrange(k):
-        y1 = -(ld.weights.item((i, 0)) - 5 * ld.weights.item((i, 1))) / ld.weights.item((i, 2))
-        y2 = -(ld.weights.item((i, 0)) + 5 * ld.weights.item((i, 1))) / ld.weights.item((i, 2))
-
-        plt.plot([-5, 5], [y1, y2], colors[i] + '--')
-
-    plt.xlim(0, 2)
-    plt.ylim(0, 2)
-    plt.show()
-
-if __name__ == '__main__':
-    main()
